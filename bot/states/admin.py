777409c55f90c12e16776c182e1e97b624d64c8a
pyadminru <zh.vitaliy92@yandex.ru>
2026-01-22 16:35:42 +0300

    Для удобства запуска и установки были добавлены: Установка.bat и Запуск.bat.

--- bot/states/admin.py
+++ bot/states/admin.py
@@ -4,4 +4,4 @@ from aiogram.fsm.state import StatesGroup, State
 
 class Admin(StatesGroup):
     id_user = State()  # id пользователя бота
-    role = State()
+    role = State()  # роль пользователя
